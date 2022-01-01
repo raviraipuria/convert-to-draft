22e1d93d832931cf7d1c65cf8cfdc0066bfaf6ed
Ravi Raipuria <ravi.raipuria@gmail.com>
2022-01-01 13:16:15 +0530

    unit test for util

--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -1,15 +1,13 @@
-import {shouldConvertToDraft} from '../src/util'
+import { shouldConvertToDraft} from '../src/util'
 import {expect, test} from '@jest/globals'
-import {countReset} from 'console'
 
 test('should allow convert to draft', async () => {
-  const now = new Date()
-  now.setDate(now.getDate() - 2)
-  console.log(now.toISOString())
-  expect(shouldConvertToDraft(now.toISOString(), 1)).toBeTruthy()
+    const now = new Date()
+    now.setDate(now.getDate()-2)
+    expect(shouldConvertToDraft(now.toISOString(), 1)).toBeTruthy()
 })
 
 test('should not allow convert to draft', async () => {
-  const now = new Date()
-  expect(shouldConvertToDraft(now.toISOString(), 1)).toBeFalsy()
+    const now = new Date()
+    expect(shouldConvertToDraft(now.toISOString(), 1)).toBeFalsy()
 })
